4712e44a98a3d13ae131d7f5b8d0b51f7427fafa
zhanghang <zhanghang1@xiaohongshu.com>
2023-11-08 18:34:18 +0800

    add :添加pixi库的适用

---
+++ pixi/pixiJS/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import * as PIXI from "pixi.js"
+//创建一个应用
+const app=new PIXI.Application({
+width:window.innerWidth,
+height:window.innerHeight,
+background:  0x16999b,
+resolution:window.devicePixelRatio || 1
+})
+document.body.appendChild(app.view)
+const rectTangle=new PIXI.Graphics()
+rectTangle.beginFill('red')
+rectTangle.drawCircle(200,100,30)
+rectTangle.endFill()
+app.stage.addChild(rectTangle)
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style scoped>
+
+</style>
